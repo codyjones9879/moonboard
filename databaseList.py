8398993ab2f0c911afb2332b82bbec1449a701d4
codyjones9879 <t.batman.t@gmail.com>
2017-07-31 16:04:31 -0500

    I added button pressing actions, giving way for updating images and loading the LED's

--- databaseList.py
+++ databaseList.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from kivy.app import App
-import pymysql
-import pymysql.cursors
+import kivy
 
-from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from kivy.uix.scrollview import ScrollView
+from kivy.core.text import LabelBase
+import pymysql
+import pymysql.cursors
 from kivy.core.window import Window
-from kivy.app import runTouchApp
 
+LabelBase.register(name="NotoSans",
+                   fn_regular="NotoSans-hinted/NotoSansUI-Regular.ttf",
+                   fn_bold="NotoSans-hinted/NotoSansUI-Bold.ttf",
+                   fn_italic="NotoSans-hinted/NotoSansUI-Italic.ttf",
+                   fn_bolditalic="NotoSans-hinted/NotoSansUI-BoldItalic.ttf")
+count = 0
 class DbCon:
-
     def __init__(self):
         self.db = pymysql.connect(host="localhost",user="root",passwd="root",db="climbingholdsape")
         self.c = self.db.cursor()
@@ -23,32 +27,22 @@ class DbCon:
         self.c.execute("SELECT * FROM Moonboard")
         return self.c.fetchall()
 
+class MyButton(Button):
+    def on_press(self):
+        print(self.text)
 
-class Table(BoxLayout):
-
-    def __init__(self,**kwargs):
-        super(Table,self).__init__(**kwargs)
-
-        self.orientation = "vertical"
-
-        self.search_field = BoxLayout(orientation="horizontal")
-
-        self.search_input = TextInput(text='search',multiline=False)
-        self.search_button = Button(text="search",on_press=self.search)
-
-        self.search_field.add_widget(self.search_input)
-        self.search_field.add_widget(self.search_button)
-
-        self.add_widget(self.search_field)
+class DisplayGridLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super(DisplayGridLayout, self).__init__(**kwargs)
 
         self.db = DbCon()
         self.Routes = self.db.get_rows()
-
+        btn = [None] * len(self.Routes)
         self.grid = GridLayout(cols=1, size_hint_y=None)
-
-        for i in range(50):
-            btn = Button(text=str(self.Routes[i][0]+'\n'+self.Routes[i][1])+'\n'+"Font Grade: "+self.Routes[i][2], size_hint_y=None)
-            self.grid.add_widget(btn)
+        self.grid.bind(minimum_height=self.grid.setter('height'))
+        for i in range(len(self.Routes)):
+            btn[i] = MyButton(text=str(self.Routes[i][0]+'\n'+self.Routes[i][1])+'\n'+"Font Grade: "+self.Routes[i][2], size_hint_y=None)
+            self.grid.add_widget(btn[i])
         self.scrolling = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
         self.scrolling.add_widget(self.grid)
         self.add_widget(self.scrolling)
@@ -58,26 +52,14 @@ class Table(BoxLayout):
 
 
 
-    def update_table(self,search=""):
-        for index,row in enumerate(self.db.get_rows(search)):
-            self.rows[index][0].text = row[0]
-            self.rows[index][1].text = str(row[1])
-            self.rows[index][2].text = str(row[2])
-
-    def clear_table(self):
-        for index in range(3):
-            self.rows[index][0].text = ""
-            self.rows[index][1].text = ""
-            self.rows[index][2].text = ""
-
-    def search(self, *args):
-        self.clear_table()
-        self.update_table(self.search_input.text)
 
 
-class MyApp(App):
+
+
+class DatabaseApp(App):
+
     def build(self):
-        return Table()
+        return DisplayGridLayout()
 
-
-MyApp().run()
+dataApp = DatabaseApp()
+dataApp.run()

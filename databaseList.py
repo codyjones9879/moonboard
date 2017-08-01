7513bccd2317a7717ce4c2800710f9db636ab2f8
codyjones9879 <t.batman.t@gmail.com>
2017-08-01 10:07:41 -0500

    added all fields for the MyButton class, enede to change names of the file and classes to make it more Moonboard themed since there will be more button elements in the final app

--- databaseList.py
+++ databaseList.py
@@ -4,7 +4,10 @@ import kivy
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.checkbox import CheckBox
+from kivy.config import Config
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.core.text import LabelBase
@@ -18,6 +21,7 @@ LabelBase.register(name="NotoSans",
                    fn_italic="NotoSans-hinted/NotoSansUI-Italic.ttf",
                    fn_bolditalic="NotoSans-hinted/NotoSansUI-BoldItalic.ttf")
 count = 0
+Window.fullscreen = 'auto'
 class DbCon:
     def __init__(self):
         self.db = pymysql.connect(host="localhost",user="root",passwd="root",db="climbingholdsape")
@@ -28,13 +32,29 @@ class DbCon:
         return self.c.fetchall()
 
 class MyButton(Button):
+    route = [None] * 205
+    routeName = ""
+    setterName = ""
+    gradeUK = ""
+    gradeUS = ""
+    stars = 0
+    moves = 0
+    repeats = 0
     def on_press(self):
-        print(self.text)
+        print(self.routeName)
+        print(self.setterName)
+        print(self.gradeUK)
+        print(self.gradeUS)
+        print(self.stars)
+        print(self.moves)
+        print(self.repeats)
+        print(self.route[0:204])
 
 class DisplayGridLayout(GridLayout):
     def __init__(self, **kwargs):
         super(DisplayGridLayout, self).__init__(**kwargs)
-
+        self.cols = 1
+        self.rows = 3
         self.db = DbCon()
         self.Routes = self.db.get_rows()
         btn = [None] * len(self.Routes)
@@ -42,11 +62,22 @@ class DisplayGridLayout(GridLayout):
         self.grid.bind(minimum_height=self.grid.setter('height'))
         for i in range(len(self.Routes)):
             btn[i] = MyButton(text=str(self.Routes[i][0]+'\n'+self.Routes[i][1])+'\n'+"Font Grade: "+self.Routes[i][2], size_hint_y=None)
+            btn[i].route = self.Routes[i][7:211]
+            btn[i].routeName = str(self.Routes[i][0])
+            btn[i].setterName = str(self.Routes[i][1])
+            btn[i].gradeUK = str(self.Routes[i][2])
+            btn[i].gradeUS = str(self.Routes[i][3])
+            btn[i].stars = self.Routes[i][4]
+            btn[i].moves = self.Routes[i][5]
+            btn[i].repeats = self.Routes[i][6]
             self.grid.add_widget(btn[i])
         self.scrolling = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+        self.filters = CheckBox()
+        self.filtered = CheckBox()
         self.scrolling.add_widget(self.grid)
         self.add_widget(self.scrolling)
-
+        self.add_widget(self.filters)
+        self.add_widget(self.filtered)
 
 
 
@@ -59,7 +90,10 @@ class DisplayGridLayout(GridLayout):
 class DatabaseApp(App):
 
     def build(self):
-        return DisplayGridLayout()
+        parent = BoxLayout(size=(Window.width, Window.height))
+        self.gridsDisplay = DisplayGridLayout()
+        parent.add_widget(self.gridsDisplay)
+        return parent
 
 dataApp = DatabaseApp()
 dataApp.run()

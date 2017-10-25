7a393da37a9fe71ad32705750a84de2080bd09f6
codyjones9879 <t.batman.t@gmail.com>
2017-10-25 15:40:02 -0500

    included BeautifulSoup

--- Moonboard_Upload_V2.py
+++ Moonboard_Upload_V2.py
@@ -1,13 +1,38 @@
 
-
+from __future__ import unicode_literals
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+# -*- coding: utf-8 -*-
+
+# encoding: utf-8
+# decoding: utf-8
+from bs4 import BeautifulSoup
+#import MySQLdb
+import pymysql
+import pymysql.cursors
+import requests
+import urllib
+import time
+import re
+import logging
+import os
+import sys
 
 
 driver = webdriver.Chrome('D:/chromedriver.exe')
 # load the page
 driver.get("https://moonboard.com/Account/Login")
+
+
+#username = driver.find_element_by_id("username")
+username = driver.find_element_by_name("Login.Username")
+password = driver.find_element_by_name("Login.Password")
+
+username.send_keys("theapedoctor")
+password.send_keys("comply9879")
+
 
 # get the submit button
 bt_submit = driver.find_element_by_css_selector("[type=submit]")
@@ -16,9 +41,18 @@ bt_submit = driver.find_element_by_css_selector("[type=submit]")
 WebDriverWait(driver, timeout=9000, poll_frequency=1) \
   .until(EC.staleness_of(bt_submit))
 
-print "submitted"
+#print "submitted"
 
-problemsButton = driver.find_element_by_id("m-viewproblem")
+problemsButton = driver.find_element_by_id("lProblems")
 problemsButton.click()
+viewProblem = driver.find_element_by_id("m-viewproblem").click()
+soupProblems = BeautifulSoup(driver.page_source, 'html.parser')
+problems = soupProblems.find_all('div')
+#print(soupProblems)
+print(problems)
 
-print problemsButton
+
+
+
+#problemList = driver.find_element_by_class_name("problem")
+#print problemList

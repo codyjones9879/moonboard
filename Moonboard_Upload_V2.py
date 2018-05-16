b9ab2c19c269320f7678e3e0ec1c031fc9a95cc5
codyjones9879 <t.batman.t@gmail.com>
2018-05-16 11:00:32 -0500

    This is now the code to simulate logging in and scanning through problems and adding the links to both the old setup and new setup, it does seem to hate having less than 10 problems per page so the last page from both groups won't get added if there is less than 10

--- Moonboard_Upload_V2.py
+++ Moonboard_Upload_V2.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 # -*- coding: utf-8 -*-
 
 # encoding: utf-8
@@ -93,42 +94,83 @@ problemsButton.click()
 viewProblem = driver.find_element_by_id("m-viewproblem").click()
 time.sleep(2)
 
-soupProblems = BeautifulSoup(driver.page_source, 'html.parser')
-#problems = soupProblems.find_all("a", href=True)
-print(soupProblems)
-# for divs in soupProblems.find_all("div", class_="problem"):
-#     for a in divs.find_all("a", href=True):
-#         print("Found the URL:", a['href'])
-index = 2
-moreProblems = True
-query = getQuery()
-while moreProblems:
-  soupProblems = BeautifulSoup(driver.page_source, 'html.parser')
-  pageClick = driver.find_element_by_css_selector('[data-page="%d"]' % index)
-  print(pageClick)
-  pageClick.click()
-  time.sleep(5)
-  for divs in soupProblems.find_all("div", class_="problem"):
-    for a in divs.find_all("a", href=True):
-      if a is None:
-        moreProblems = False
-      else:
-        query = getQuery()
-
-        link = a['href']
-        submitDB(db, query, link)
-        problemsLinkArray.append(a['href'])
-        #linkIndex+=1
-        print("Found the URL:", a['href'])
-    #print(pages)
-  #print(problemsLinkArray)
-  index+=1
 
 
 
+try:
+    soupProblems = BeautifulSoup(driver.page_source, 'html.parser')
+    #problems = soupProblems.find_all("a", href=True)
+    print(soupProblems)
+    # for divs in soupProblems.find_all("div", class_="problem"):
+    #     for a in divs.find_all("a", href=True):
+    #         print("Found the URL:", a['href'])
+    index = 2
+    moreProblems = True
+    query = getQuery()
+    while moreProblems:
+      soupProblems = BeautifulSoup(driver.page_source, 'html.parser')
+      pageClick = driver.find_element_by_css_selector('[data-page="%d"]' % index)
+      print(pageClick)
+      pageClick.click()
+      time.sleep(5)
+      for divs in soupProblems.find_all("div", class_="problem"):
+        for a in divs.find_all("a", href=True):
+          if a is None:
+            moreProblems = False
+          else:
+            query = getQuery()
+
+            link = a['href']
+            submitDB(db, query, link)
+            problemsLinkArray.append(a['href'])
+            #linkIndex+=1
+            print("Found the URL:", a['href'])
+        #print(pages)
+      #print(problemsLinkArray)
+      index+=1
+except:
+    print("Done1")
 
 
-#problemList = driver.find_element_by_class_name("problem")
-#print problemList
 
-#<div class="k-grid-content" style="height: 752.563px;"><table role="grid"><colgroup></colgroup><tbody role="rowgroup"><tr data-uid="308025" onclick="problemSelected();" class="selected"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308025/hyul-001" target="_blank">HYUL 001</a></h3><p>            ki won Nam        </p>        <p>            Be the first to repeat this problem           </p>        <p>            6B+        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308024" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308024/chundercut" target="_blank">CHUNDERCUT</a></h3><p>            Remus Knowles        </p>        <p>            Be the first to repeat this problem           </p>        <p>            7B        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308023" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308023/kreuzung" target="_blank">KREUZUNG</a></h3><p>            Danny Maldener        </p>        <p>            Be the first to repeat this problem           </p>        <p>            6B+        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308022" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308022/judgement-rains" target="_blank">JUDGEMENT RAINS</a></h3><p>            Gus Carter        </p>        <p>            Be the first to repeat this problem           </p>        <p>            7B+        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308021" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308021/second-place-1st-lou-ser" target="_blank">SECOND PLACE, 1ST LOU-SER</a></h3><p>            Gus Carter        </p>        <p>            Be the first to repeat this problem           </p>        <p>            7C+        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308020" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308020/right-left-up-down" target="_blank">RIGHT, LEFT, UP, DOWN</a></h3><p>            Tim Teylan        </p>        <p>            Be the first to repeat this problem           </p>        <p>            6C+        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308019" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308019/dyno-fit" target="_blank">DYNO FIT</a></h3><p>            Chuchi Climber        </p>        <p>            Be the first to repeat this problem           </p>        <p>            7C        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308018" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308018/crossword" target="_blank">CROSSWORD</a></h3><p>            Jon Pål Hamre        </p>        <p>            Be the first to repeat this problem           </p>        <p>            7C        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308017" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308017/habibi-direkt" target="_blank">HABIBI DIREKT</a></h3><p>            Daniel Pliegl        </p>        <p>            Be the first to repeat this problem           </p>        <p>            7A+        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308016" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308016/test-a1" target="_blank">TEST A1</a></h3><p>            iTTE Climbing        </p>        <p>            Be the first to repeat this problem           </p>        <p>            7A        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308015" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308015/ror" target="_blank">ROR</a></h3><p>            abi        </p>        <p>            Be the first to repeat this problem           </p>        <p>            6C        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308014" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308014/oh-rock-beautiful" target="_blank">OH ROCK BEAUTIFUL</a></h3><p>            abi        </p>        <p>            Be the first to repeat this problem           </p>        <p>            6C        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308013" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308013/fun-stuff" target="_blank">FUN STUFF</a></h3><p>            Tauty        </p>        <p>            Be the first to repeat this problem           </p>        <p>            6C+        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308012" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308012/daipanman" target="_blank">DAIPANMAN</a></h3><p>            DAI NUMATA        </p>        <p>            Be the first to repeat this problem           </p>        <p>            6C        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr><tr data-uid="308011" onclick="problemSelected();"><td><div class="problem">      <div class="problem-inner">        <h3><a href="/Problems/View/308011/たい" target="_blank">たい</a></h3><p>            DAI NUMATA        </p>        <p>            Be the first to repeat this problem           </p>        <p>            6B+        </p><p>Feet follow hands</p><ul><li>    <img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li><li><img src="/Content/images/starempty.png"></li></ul>   <p class="bold">  </p></div><div class="add"><button class="moon-roundbutton"><img src="/Content/images/Buttons/add.png"></button><span>Add</span>       </div></div></td></tr></tbody></table></div>
+
+
+viewProblems2016 = Select(driver.find_element_by_id('Holdsetup'))
+viewProblems2016.select_by_visible_text('MoonBoard 2016')
+time.sleep(2)
+
+try:
+    soupProblems = BeautifulSoup(driver.page_source, 'html.parser')
+    # problems = soupProblems.find_all("a", href=True)
+    print(soupProblems)
+    # for divs in soupProblems.find_all("div", class_="problem"):
+    #     for a in divs.find_all("a", href=True):
+    #         print("Found the URL:", a['href'])
+    index = 2
+    moreProblems = True
+    query = getQuery()
+    while moreProblems:
+        soupProblems = BeautifulSoup(driver.page_source, 'html.parser')
+        pageClick = driver.find_element_by_css_selector('[data-page="%d"]' % index)
+        print(pageClick)
+        pageClick.click()
+        time.sleep(5)
+        for divs in soupProblems.find_all("div", class_="problem"):
+            for a in divs.find_all("a", href=True):
+                if a is None:
+                    moreProblems = False
+                else:
+                    query = getQuery()
+
+                    link = a['href']
+                    submitDB(db, query, link)
+                    problemsLinkArray.append(a['href'])
+                    # linkIndex+=1
+                    print("Found the URL:", a['href'])
+                    # print(pages)
+        # print(problemsLinkArray)
+        index += 1
+except:
+    print("Done2")
+
+
